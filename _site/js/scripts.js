8ba141fb720de061099e7f67dee02bd77b33fa0e
John Prefer <jprefer@gmail.com>
2020-06-15 15:31:37 -0400

    just an update

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -30,9 +30,9 @@
 			$(".fixed-top").removeClass("top-nav-collapse");
 		}
     });
-
+ // Taken out 0615/2020
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
+	/*$(function() {
 		$(document).on('click', 'a.page-scroll', function(event) {
 			var $anchor = $(this);
 			$('html, body').stop().animate({
@@ -41,7 +41,7 @@
 			event.preventDefault();
 		});
 	});
-
+*/
     // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
     if (!$(this).parent().hasClass('dropdown'))
